a86f647e02927d0a5e4f7ae4062d374c297f4326
Christian Brandon Neri Sanchez <cbneris@gmail.com>
2025-06-05 14:49:29 -0600

    Se agrega la validación para detectar la actividad de la sesión, así como función para cerrar la sesión

--- src/app/Components/estrustura-web/nav/nav.ts
+++ src/app/Components/estrustura-web/nav/nav.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Sesion } from '../../../service/sesion';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-nav',
@@ -7,5 +9,24 @@ import { Component } from '@angular/core';
   styleUrl: './nav.css'
 })
 export class Nav {
+
+  usuario: String = "Usuario de la Sesión";
+
+  constructor(
+      private router:   Router,
+      private sesionCk: Sesion
+    ){}
+  
+  ngOnInit(): void {
+    this.usuario = this.sesionCk.obtenerNombreUsuario();
+    if(!this.sesionCk.sesionActiva()){
+      this.router.navigate(['/login']);
+    }
+  }
+
+  cerrarSesion(){
+    this.sesionCk.cerrarSesion();
+    this.router.navigate(['/login']);
+  }
 
 }
