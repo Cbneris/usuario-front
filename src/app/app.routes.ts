489a59741eecae83ba88c29c265cc460b3d94081
Christian Brandon Neri Sanchez <cbneris@gmail.com>
2025-06-05 09:58:13 -0600

    Define application routes for login and home components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { Login } from './Components/login/login';
+import { Home } from './Components/home/home';
 
-export const routes: Routes = [];
+export const appRoutes: Routes = [
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: 'login', component: Login },
+  { path: 'home', component: Home }
+];
