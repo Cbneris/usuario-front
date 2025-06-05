fedb32c937ee5442cd4d1822ccd5b0ef2400fcae
Christian Brandon Neri Sanchez <cbneris@gmail.com>
2025-06-05 09:56:36 -0600

    Agregar servicio TransaccionesHTTP para manejar la lógica de inicio de sesión

---
+++ src/app/service/transacciones-http.ts
@@ -0,0 +1,20 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { UsuarioLoginRequest } from '../Model/UsuarioLoginRequest';
+import { RequestMapping } from '../Util/Constantes/RequestMapping';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TransaccionesHTTP {
+  
+  private baseLocalUrl = 'http://localhost:8080';  
+  request: RequestMapping = new RequestMapping();
+
+  constructor(private http: HttpClient) {}
+
+  iniciarSesion( user : UsuarioLoginRequest ){
+    return this.http.post(  `${ this.baseLocalUrl }${this.request.LoginController}${this.request.LoginUsuario}`, user ).toPromise();
+  }
+
+}
