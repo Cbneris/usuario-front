773561f67ddcc04e64f38fa4bc71f0d183c432f9
Christian Brandon Neri Sanchez <cbneris@gmail.com>
2025-06-05 16:05:49 -0600

    Se agrega la ruta del componente de gestion de usuarios

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { Login } from './Components/login/login';
 import { Home } from './Components/home/home';
+import { GestionUsuarios } from './Components/gestion-usuarios/gestion-usuarios';
 
 export const appRoutes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
   { path: 'login', component: Login },
-  { path: 'home', component: Home }
+  { path: 'home', component: Home },
+  { path: 'gestion-usuarios', component: GestionUsuarios }
 ];
