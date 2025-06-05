3de879fd18ca52c613ff0c547df938edb438dd06
Christian Brandon Neri Sanchez <cbneris@gmail.com>
2025-06-05 16:05:07 -0600

    Se crea el componente de gestion de usuarios y se le agrega el formulario

---
+++ src/app/Components/gestion-usuarios/gestion-usuarios.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { Nav } from '../estrustura-web/nav/nav';
+
+@Component({
+  selector: 'app-gestion-usuarios',
+  imports: [Nav],
+  templateUrl: './gestion-usuarios.html',
+  styleUrl: './gestion-usuarios.css'
+})
+export class GestionUsuarios {
+
+}
