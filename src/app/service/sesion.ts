737eef84a36980ac6a59ef87f04530b113f820fa
Christian Brandon Neri Sanchez <cbneris@gmail.com>
2025-06-05 10:34:56 -0600

    Se crea el servicio de manejo de cookies

---
+++ src/app/service/sesion.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@angular/core';
+import { CookieService } from 'ngx-cookie-service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class Sesion {
+  
+  constructor(private cookieService: CookieService) {}
+  
+  guardarSesion(login: string, nombreUsuario: string): void {
+    this.cookieService.set('login', login);
+    this.cookieService.set('nombreUsuario', nombreUsuario);
+  }
+
+  obtenerLogin(): string {
+    return this.cookieService.get('login');
+  }
+
+  obtenerNombreUsuario(): string {
+    return this.cookieService.get('nombreUsuario');
+  }
+
+  cerrarSesion(): void {
+    this.cookieService.delete('login');
+    this.cookieService.delete('nombreUsuario');
+  }
+
+  sesionActiva(): boolean {
+    return this.cookieService.check('login') && this.cookieService.check('nombreUsuario');
+  }
+
+}
