5634e7c613357b95c385418b5b7144ec676dc71c
Christian Brandon Neri Sanchez <cbneris@gmail.com>
2025-06-06 09:57:12 -0600

    Se crea el formulario del usuario y se integra al controlador de la cista html, que se le agega el formgroup

--- src/app/Components/gestion-usuarios/gestion-usuarios.ts
+++ src/app/Components/gestion-usuarios/gestion-usuarios.ts
@@ -1,12 +1,30 @@
-import { Component } from '@angular/core';
-import { Nav } from '../estrustura-web/nav/nav';
+import { Nav }                    from '../estrustura-web/nav/nav';
+import { Component }              from '@angular/core';
+import { USUARIO_FORM }           from '../../Util/Forms/usuarioForm';
+import { FormBuilder, FormGroup, ReactiveFormsModule  } from '@angular/forms';
 
 @Component({
   selector: 'app-gestion-usuarios',
-  imports: [Nav],
+  imports: [Nav, ReactiveFormsModule],
   templateUrl: './gestion-usuarios.html',
   styleUrl: './gestion-usuarios.css'
 })
 export class GestionUsuarios {
+  usuarioForm!: FormGroup;
 
+  constructor(    
+    private fb: FormBuilder
+  ){}
+
+  ngOnInit(): void {
+    this.configurarCamposUsuario();
+  }
+
+  configurarCamposUsuario(){
+    this.usuarioForm = this.fb.group( USUARIO_FORM );
+  }
+
+  guardaUsuario(){
+    console.log(this.usuarioForm.getRawValue());    
+  }
 }
