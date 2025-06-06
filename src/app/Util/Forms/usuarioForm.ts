ddbc7649b76961d4b9e8bd454d1b933bb8be0a62
Christian Brandon Neri Sanchez <cbneris@gmail.com>
2025-06-06 15:55:27 -0600

    Se agrega la validación de digitos en el cliente

--- src/app/Util/Forms/usuarioForm.ts
+++ src/app/Util/Forms/usuarioForm.ts
@@ -4,7 +4,7 @@ export const USUARIO_FORM = {
     login:              ['', Validators.required],
     password:           ['', Validators.required],
     nombre:             ['', Validators.required],
-    cliente:            ['', Validators.required],
+    cliente:            ['', Validators.required, Validators.pattern(/^\d+(\.\d{1,2})?$/)], //Patrón para validar decimales
     email:              ['', [Validators.required, Validators.email]],
     intentos:           ['', Validators.required],
     fechaRevocado:      [''],
