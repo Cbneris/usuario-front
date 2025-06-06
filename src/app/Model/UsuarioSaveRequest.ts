18f565f4213fa58087fe6fb43df129063f7c87cd
Christian Brandon Neri Sanchez <cbneris@gmail.com>
2025-06-06 17:14:32 -0600

    Creación del usuario en respuesta del Back

---
+++ src/app/Model/UsuarioSaveRequest.ts
@@ -0,0 +1,16 @@
+export class UsuarioSaveRequest {
+
+    login?:             String;
+    password?:          String;
+    nombre?:            String;
+    cliente?:           number;
+    email?:             String;
+    intentos?:          number;
+    fechaRevocado?:     Date;
+    fechaVigencia?:     Date;
+    noAcceso?:          number;
+    apellidoPaterno?:   String;
+    apellidoMaterno?:   String;
+    area?:              String;
+
+}
