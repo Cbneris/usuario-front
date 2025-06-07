7931d70750cf853c764131f1b68ebaf36d1fc853
Christian Brandon Neri Sanchez <cbneris@gmail.com>
2025-06-06 20:54:36 -0600

    Se crea el elemento de los titulos del mensaje

---
+++ src/app/Util/Alerta/TituloMsg.ts
@@ -0,0 +1,10 @@
+export const TITULO = {
+
+        ERROR:                 "ERROR",
+        ALERTA:                "ALERTA", 
+        ATENCIÓN:              "ATENCIÓN", 
+        EXITO:                 "EXITO!!", 
+        DISCULPA:              "LO SENTIMOS",
+        BIENVENIDA:            "Bienvenido",
+        
+}
