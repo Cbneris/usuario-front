e78eda93a16b97ba0f17c70efd3950e3149b4b12
Christian Brandon Neri Sanchez <cbneris@gmail.com>
2025-06-08 13:32:26 -0600

    Se agrega la carga de usuarios

--- src/app/Components/tablero-buscador-usuarios/tablero-buscador-usuarios.ts
+++ src/app/Components/tablero-buscador-usuarios/tablero-buscador-usuarios.ts
@@ -1,11 +1,38 @@
-import { Component } from '@angular/core';
+import { Nav }                from '../estrustura-web/nav/nav';
+import { Usuario }            from '../../Model/Usuario';
+import { Component }          from '@angular/core';
+import { CommonModule }       from '@angular/common';
+import { TransaccionesHTTP }  from '../../service/transacciones-http';
 
 @Component({
   selector: 'app-tablero-buscador-usuarios',
-  imports: [],
+  imports: [Nav, CommonModule],
   templateUrl: './tablero-buscador-usuarios.html',
   styleUrl: './tablero-buscador-usuarios.css'
 })
+
 export class TableroBuscadorUsuarios {
+  
+  listUsuarios:   Usuario [] = [];
+
+  constructor(
+      private transaccionesService: TransaccionesHTTP
+  ){}
+
+  ngOnInit(): void {
+    this.cargaListaUsuarios();
+  }
+
+  cargaListaUsuarios() {
+    this.transaccionesService.cargaListUsuarios()
+    .then((response : any) => {
+      console.log(response.entity);
+      this.listUsuarios = response.entity
+    })
+  }
+
+  busquedaStatusA(){
+    
+  }
 
 }
