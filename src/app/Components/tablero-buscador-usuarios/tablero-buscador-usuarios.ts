d140d6ff1325899875861a5cc1941e8d62a501b0
Christian Brandon Neri Sanchez <cbneris@gmail.com>
2025-06-07 20:49:59 -0600

    Se crea el componente para el tablero buscador de usuarios

---
+++ src/app/Components/tablero-buscador-usuarios/tablero-buscador-usuarios.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tablero-buscador-usuarios',
+  imports: [],
+  templateUrl: './tablero-buscador-usuarios.html',
+  styleUrl: './tablero-buscador-usuarios.css'
+})
+export class TableroBuscadorUsuarios {
+
+}
