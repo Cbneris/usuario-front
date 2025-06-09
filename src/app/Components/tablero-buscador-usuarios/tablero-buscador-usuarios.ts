65581a810b1cdf5e911e4f1f4e02d570fbad73b9
Christian Brandon Neri Sanchez <cbneris@gmail.com>
2025-06-08 20:34:17 -0600

    Se agregan las variables para los filtros de búsqueda Se agrega la funcion para limpiar los filtros Se crean los metodos para convertir el formato de las fechas alta y baja Se crea el metodo para buscar por cualquiera de los tre filtros

--- src/app/Components/tablero-buscador-usuarios/tablero-buscador-usuarios.ts
+++ src/app/Components/tablero-buscador-usuarios/tablero-buscador-usuarios.ts
@@ -3,15 +3,23 @@ import { Usuario }            from '../../Model/Usuario';
 import { Component }          from '@angular/core';
 import { CommonModule }       from '@angular/common';
 import { TransaccionesHTTP }  from '../../service/transacciones-http';
+import { FormsModule } from '@angular/forms';
 
 @Component({
-  selector: 'app-tablero-buscador-usuarios',
-  imports: [Nav, CommonModule],
-  templateUrl: './tablero-buscador-usuarios.html',
-  styleUrl: './tablero-buscador-usuarios.css'
+  selector:     'app-tablero-buscador-usuarios',
+  imports:      [Nav, CommonModule, FormsModule],
+  templateUrl:  './tablero-buscador-usuarios.html',
+  styleUrl:     './tablero-buscador-usuarios.css'
 })
 
 export class TableroBuscadorUsuarios {
+
+  nombreFiltro: string = '';
+  fechaAltaStr: string = '';
+  fechaBajaStr: string = '';
+
+  fechaAlta:    Date | null = null;
+  fechaBaja:    Date | null = null;
   
   listUsuarios:           Usuario[] = [];
   listaOriginalUsuarios:  Usuario[] = [];
@@ -24,6 +32,14 @@ export class TableroBuscadorUsuarios {
     this.cargaListaUsuarios();
   }
 
+  limpiafiltros(){
+    this.nombreFiltro = '';
+    this.fechaAltaStr = '';
+    this.fechaBajaStr = '';
+
+    this.muestraListaOriginal();
+  }
+
   cargaListaUsuarios() {
     this.transaccionesService.cargaListUsuarios()
     .then((response : any) => {
@@ -32,12 +48,52 @@ export class TableroBuscadorUsuarios {
     })
   }
 
+  onFechaAltaChange() {
+    this.fechaAlta = this.fechaAltaStr ? new Date(this.fechaAltaStr) : null;    
+  }
+
+  onFechaBajaChange() {
+    this.fechaBaja = this.fechaBajaStr ? new Date(this.fechaBajaStr) : null;
+  }
+
   filtrarPorStatus(status: string) {
     this.listUsuarios = this.listaOriginalUsuarios.filter(usuario => usuario.status === status);
   }
 
-  limpiafiltros(){
+  muestraListaOriginal(){
     this.listUsuarios = this.listaOriginalUsuarios;
+  }
+
+  busquedaPorFiltros(){
+    const nombreFiltro = this.nombreFiltro.trim().toLowerCase();
+
+    //Carga la lista original previamente consultada sí no ay filtros llenos
+    if(!nombreFiltro && !this.fechaAlta && !this.fechaBaja){
+      this.listUsuarios = [...this.listaOriginalUsuarios];
+      return;
+    }
+
+    this.listUsuarios = this.listaOriginalUsuarios.filter(usuario => {
+      const nombreCoincide = this.nombreFiltro
+        ? usuario.nombre?.toLowerCase().includes(this.nombreFiltro.toLowerCase()) ?? false
+        : true;
+      
+      const fechaAltaUsuario = usuario.fechaAlta ? new Date(usuario.fechaAlta) : null;
+      const fechaAltaCoincide = this.fechaAlta
+        ? fechaAltaUsuario && this.sonMismasFechas(fechaAltaUsuario, this.fechaAlta)
+        : true;
+
+      const fechaBajaUsuario = usuario.fechaBaja ? new Date(usuario.fechaBaja) : null;
+      const fechaBajaCoincide = this.fechaBaja
+        ? fechaBajaUsuario && this.sonMismasFechas(fechaBajaUsuario, this.fechaBaja)
+        : true;
+
+      return nombreCoincide && fechaAltaCoincide && fechaBajaCoincide;
+    });
+  }
+
+  sonMismasFechas(fecha1: Date, fecha2: Date): boolean {
+    return fecha1.toISOString().slice(0, 10) === fecha2.toISOString().slice(0, 10);
   }
 
 }
